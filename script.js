fa6b88d3f571e06ebe19fa3af0ebd4d437e6ba05
dptapcode <dacphong207@gmail.com>
2026-09-25 06:32:38 +0700

    f

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const txt = "Hôm nay là Tết Trung Thu, chúc cho ngdep halinh thật nhiều vui vẻ và hạnh phúc nhee !!! Chúc Linh luôn rực rỡ như ánh trăng rằm, lúc nào cũng xinh điẹp, cute, khuôn trăng đầy đặn, chỗ nào cũng nở nang.  Nhưng mà rực rỡ vừa thoi không ngta tưởng chị Hằng xuống trần gian nhé. Chúc Linh sẽ luôn giữ được sự vô tư hài hài mát mát yêu đời, mang lại năng lượng tích cực cho mọi người, và Linh cũng cười thật nhiều và thật vui nhé. Thoi cũng hơi cringe ròi, t văn không hay chữ cũng chẳng tốt, viết đến đây thoi. Mong là bức thư này sẽ làm Linh cười.";
+    const txt = "Hôm nay là Tết Trung Thu, chúc cho ngdep halinh thật nhiều vui vẻ và hạnh phúc nhee!!! Chúc Linh luôn rực rỡ như ánh trăng rằm, lúc nào cũng xinh điẹp, cute, khuôn trăng đầy đặn, chỗ nào cũng nở nang. Nhưng mà rực rỡ vừa thoi không ngta tưởng chị Hằng xuống trần gian nhé. Chúc Linh sẽ luôn giữ được sự vô tư hài hài mát mát yêu đời, mang lại năng lượng tích cực cho mọi người, và Linh cũng cười thật nhiều và thật vui nhé. Thoi cũng hơi cringe ròi, t văn không hay chữ cũng chẳng tốt, viết đến đây thoi. Mong là bức thư này sẽ làm Linh cười. Chúc b Linh Trung Thu vui vẻ nhé <3";
     const tg = document.getElementById('typewriter-text');
     let i = 0;
 
@@ -40,6 +40,11 @@ document.addEventListener('DOMContentLoaded', () => {
         "img/anh8.jpeg", 
         "img/anh9.jpeg", 
         "img/anh10.jpeg", 
+        "img/anh11.jpeg", 
+        "img/anh12.jpeg", 
+        "img/anh13.jpeg", 
+        "img/anh14.jpeg", 
+        "img/anh15.jpeg", 
     ];
 
     const vung = document.getElementById('vung');
@@ -49,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
         a.className = "aroi";
         a.src = m_anh[Math.floor(Math.random() * m_anh.length)];
         
-        const kt = Math.random() * 40 + 30;
+        const kt = Math.random() * 60 + 50;
         a.style.width = kt + "px";
         a.style.height = kt + "px";
         a.style.left = Math.random() * 90 + 5 + "vw";
